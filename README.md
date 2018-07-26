e1175404c2c963afae926d0ddc9bfeef856e1351
albanie <albanie@robots.ox.ac.uk>
2018-07-26 17:19:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+mcnPathological
+---
+
+This codebase contains some "toy" optimisation problems that exhibit pathological behaviour.
---
+++ setup_mcnPathological.m
@@ -0,0 +1,9 @@
+function setup_mcnPathological()
+%SETUP_MCNPATHOLOGICAL Sets up mcnPathological, by adding its folders
+% to the MATLAB path
+%
+% Copyright (C) 2018 Samuel Albanie, Sebastien Erhardt, Joao F. Henriques
+% Licensed under The MIT License [see LICENSE.md for details]
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root, [root '/examples'], [root '/exps'], [root '/vis']) ;
