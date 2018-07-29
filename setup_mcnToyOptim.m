5ce9bf99b203c034f5f042acec38e0e414f74eb5
albanie <albanie@robots.ox.ac.uk>
2018-07-29 23:22:11 +0100

    mods

--- setup_mcnToyOptim.m
+++ setup_mcnToyOptim.m
@@ -5,6 +5,44 @@ function setup_mcnToyOptim()
 % Copyright (C) 2018 Samuel Albanie, Sebastien Erhardt, Joao F. Henriques
 % Licensed under The MIT License [see LICENSE.md for details]
 
+  % add dependencies
+  check_dependency('autonn') ;
+
   root = fileparts(mfilename('fullpath')) ;
   addpath(root, [root '/examples'], [root '/exps']) ;
   addpath([root '/matlab'], [root '/vis']) ;
+
+% -----------------------------------
+function check_dependency(moduleName)
+% -----------------------------------
+
+  name2path = @(name) strrep(name, '-', '_') ;
+  setupFunc = ['setup_', name2path(moduleName)] ;
+  if exist(setupFunc, 'file')
+    vl_contrib('setup', moduleName) ;
+  else
+    % try adding the module to the path
+    addpath(fullfile(vl_rootnn, 'contrib', moduleName)) ;
+    if exist(setupFunc, 'file')
+      vl_contrib('setup', moduleName) ;
+    else
+      waiting = true ;
+      msg = ['module %s was not found on the MATLAB path. Would you like ' ...
+             'to install it now? (y/n)\n'] ;
+      prompt = sprintf(msg, moduleName) ;
+      while waiting
+        str = input(prompt,'s') ;
+        switch str
+          case 'y'
+            vl_contrib('install', moduleName) ;
+            vl_contrib('compile', moduleName) ;
+            vl_contrib('setup', moduleName) ;
+            return ;
+          case 'n'
+            throw(exception) ;
+          otherwise
+            fprintf('input %s not recognised, please use `y` or `n`\n', str) ;
+        end
+      end
+    end
+  end
